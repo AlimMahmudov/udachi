ea8cb4862ca994e8bbd2649becd98d12054fbc18
alim <you@example.com>
2023-12-04 22:28:53 +0600

    first project

---
+++ script.js
@@ -0,0 +1,197 @@
+const body = document.body;
+
+const container = document.querySelector(".container");
+const hero = document.querySelector(".hero");
+const link = document.querySelector(".link");
+const heroContent = document.querySelector(".hero-content");
+const heroContentImg = document.querySelector(".hero-content img");
+const heroContentH1 = document.querySelector(".hero-content h1");
+const heroContentH3 = document.querySelector(".hero-content h3");
+const heroContentH4 = document.querySelector(".hero-content h4");
+const heroContentH2 = document.querySelector(".hero-content h2");
+
+///////////////////////////////////////////
+const tt = document.querySelector(".tt");
+const tth5 = document.querySelector(".tt h5");
+
+const container1 = document.querySelector(".container1");
+const block = document.querySelector(".block");
+const imageImg = document.querySelector(".image img");
+const text = document.querySelector(".text");
+const texth1 = document.querySelector(".text h1");
+const texth2 = document.querySelector(".text h2");
+const textp = document.querySelector(".text p");
+const texth3 = document.querySelector(".text h3");
+const texth4 = document.querySelector(".text h4");
+
+///////////////////////////////////////////
+
+const container2 = document.querySelector(".container2");
+const block1 = document.querySelector(".block1");
+const image1Img = document.querySelector(".image1 img");
+const text1 = document.querySelector(".text1");
+const text1h1 = document.querySelector(".text1 h1");
+const text1h2 = document.querySelector(".text1 h2");
+const text1p = document.querySelector(".text1 p");
+const text1h3 = document.querySelector(".text1 h3");
+const text1h4 = document.querySelector(".text1 h4");
+const bb = document.querySelector(".bb");
+const bbbutton = document.querySelector(".bb button");
+
+///////////////////////////////////////////
+
+const container3 = document.querySelector(".container3");
+const models2 = document.querySelector(".models2");
+const text2h2 = document.querySelector(".text2 h2");
+const text2h3 = document.querySelector(".text2 h3");
+const text3h3 = document.querySelector(".text3 h3");
+
+////////////////////////////////////////////
+
+const image2Img = document.querySelector(".image2 img");
+const block2 = document.querySelector(".block2");
+
+///////////////////////////////////////////
+
+body.style.fontFamily = "sans-serif";
+
+heroContent.style.background = "#EFF1F0";
+
+container.style.maxWidth = "700px";
+container.style.width = "100%";
+container.style.margin = "0 auto";
+container.style.padding = "0 20px";
+
+hero.style.alignItems = "center";
+hero.style.padding = "50px 0";
+
+link.style.display = "flex";
+link.style.justifyContent = "space-between";
+link.style.justifyContent = "space-around";
+link.style.marginTop = "50px";
+
+heroContentImg.style.marginLeft = "270px";
+heroContentImg.style.marginTop = "20px";
+
+heroContentH1.style.color = "#28009B";
+heroContentH1.style.fontSize = "32px";
+heroContentH1.style.textAlign = "center";
+heroContentH1.style.marginTop = "50px";
+
+heroContentH3.style.fontSize = "25px";
+heroContentH3.style.textAlign = "center";
+heroContentH3.style.marginTop = "50px";
+
+// heroContentH4.style.color = "#9D147F";
+heroContentH4.style.fontSize = "20px";
+heroContentH4.style.textAlign = "center";
+heroContentH4.style.marginTop = "10px";
+
+//////////////////////////////////////////////////////////
+tt.style.background = "#94A3B8";
+tt.style.width = "700px";
+tt.style.height = "50px";
+
+tth5.style.color = "#94A3B8";
+tth5.style.fontSize = "20px";
+tth5.style.marginLeft = "50px";
+
+//////////////////////////////////////////////
+
+block.style.background = "#94A3B8";
+block.style.display = "flex";
+block.style.marginTop = "-67px";
+
+container1.style.maxWidth = "700px";
+container1.style.width = "100%";
+container1.style.margin = "0 auto";
+container1.style.padding = "0 20px";
+
+imageImg.style.width = "180px";
+imageImg.style.height = "240px";
+imageImg.style.marginLeft = "70px";
+
+text.style.background = "#94A3B8";
+
+texth1.style.color = "#384860";
+texth1.style.fontSize = "25px";
+
+texth2.style.color = "#384860";
+texth2.style.fontSize = "25px";
+textp.style.color = "white";
+textp.style.fontSize = "17px";
+
+texth3.style.color = "#94A3B8";
+texth3.style.fontSize = "15px";
+
+texth4.style.color = "#060368";
+texth4.style.fontSize = "15px";
+texth4.style.marginLeft = "300px";
+
+//////////////////////////////////////////////////////////
+
+block1.style.background = "#94A3B8";
+block1.style.display = "flex";
+block1.style.marginTop = "3px";
+
+container2.style.maxWidth = "700px";
+container2.style.width = "100%";
+container2.style.margin = "0 auto";
+container2.style.padding = "0 20px";
+
+image1Img.style.width = "170px";
+image1Img.style.height = "240px";
+image1Img.style.marginLeft = "70px";
+
+text1.style.background = "#94A3B8";
+text1.style.marginLeft = "10px";
+
+text1h1.style.color = "#384860";
+text1h1.style.fontSize = "25px";
+
+text1h2.style.color = "#384860";
+text1h2.style.fontSize = "25px";
+
+text1p.style.color = "white";
+text1p.style.fontSize = "17px";
+
+text1h3.style.color = "#94A3B8";
+text1h3.style.fontSize = "15px";
+
+text1h4.style.color = "#060368";
+text1h4.style.fontSize = "15px";
+text1h4.style.marginLeft = "300px";
+
+//////////////////////////////////////////////
+
+bb.style.background = "#94A3B8";
+bb.style.width = "700px";
+bb.style.height = "80px";
+
+bbbutton.style.color = "#94A3B8";
+bbbutton.style.fontSize = "20px";
+bbbutton.style.marginLeft = "250px";
+bbbutton.style.marginTop = "10px";
+bbbutton.style.width = "181px";
+bbbutton.style.height = "50px";
+bbbutton.style.color = "white";
+bbbutton.style.background = "#060368";
+bbbutton.style.border = "none";
+
+///////////////////////////////////////////////
+
+block2.style.background = "#EFF1F0";
+
+container3.style.maxWidth = "700px";
+container3.style.width = "100%";
+container3.style.margin = "0 auto";
+container3.style.padding = "0 20px";
+
+models2.style.background = "yellow";
+models2.style.marginTop = "-20px";
+
+text2h2.style.textAlign = "center";
+text2h3.style.textAlign = "center";
+text3h3.style.textAlign = "center";
+
+image2Img.style.marginLeft = "270px";
